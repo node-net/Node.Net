08b146c50735c1c234fabc4541de4ec2206fa33f
lou-parslow <lou.parslow@gmail.com>
2015-09-17 08:15:34 -0600

    'all'

--- rakefile.rb
+++ rakefile.rb
@@ -16,7 +16,7 @@ task :increment_version do
 	end
 end
 
-vs_version=MSBuild.get_vs_version('Node.Net.Test.sln')
+#vs_version=MSBuild.get_vs_version('Node.Net.Test.sln')
 #COMMANDS[:build] = ["\"#{MSBuild.get_version(vs_version)}\" Node.Net.Test.sln /nologo /p:Configuration=Release /p:Platform=\"Any CPU\"",
 #	                "\"#{MSBuild.get_version(vs_version)}\" Node.Net.Test.sln /nologo /p:Configuration=Debug /p:Platform=\"Any CPU\"",
 #					"\"#{MSBuild.get_version(vs_version)}\" Node.Net.Libs.sln /nologo /p:Configuration=Release /p:Platform=\"Any CPU\"",
@@ -24,7 +24,7 @@ vs_version=MSBuild.get_vs_version('Node.Net.Test.sln')
 #COMMANDS[:test] = ["\"#{Test.nunit_console}\" \"#{Rake.application.original_dir}/bin/Net4.6/Release/Node.Net.Test.dll\" /xml:\"bin/Net4.6/Release/Node.Net.Test.dll.TestResults.xml\""]
 task :build 
 task :publish => [:build,:test,:commit] do
-	puts "publish #{__FILE__}}"
+	#puts "publish #{__FILE__}}"
 	if(__FILE__.include?('/wrk/'))
 		Git.tag File.dirname(__FILE__), VERSION
 		#source=FileList.new('Node.Net.sln','Node.Net/**/*.{csproj,snk,cs,json,xaml}',
@@ -33,11 +33,11 @@ task :publish => [:build,:test,:commit] do
 		#Git.publish "https://github.com/lou-parslow/Node.Net.git" ,File.dirname(__FILE__), source, VERSION
 	end
 
-	Text.replace_in_file('Node.Net.nuspec',/<version>(\d+.\d+.\d+)<\/version>/,"<version>#{VERSION}</version>")
+	#Text.replace_in_file('Node.Net.nuspec',/<version>(\d+.\d+.\d+)<\/version>/,"<version>#{VERSION}</version>")
 
-	FileUtils.mkdir('lib') if !File.exists?('lib')
-	FileUtils.mkdir('lib/Net4.6') if !File.exists?('lib/Net4.6')
-	FileUtils.cp('bin/Net4.6/Release/Node.Net.dll','lib/Net4.6/Node.Net.dll')
+	#FileUtils.mkdir('lib') if !File.exists?('lib')
+	#FileUtils.mkdir('lib/Net4.6') if !File.exists?('lib/Net4.6')
+	#FileUtils.cp('bin/Net4.6/Release/Node.Net.dll','lib/Net4.6/Node.Net.dll')
 	puts `nuget pack Node.Net.nuspec`
 	puts `nuget push Node.Net.#{VERSION}.nupkg`
 end
