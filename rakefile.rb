e2d514bf7ba9dd2a201609e5b4e42a5643d463af
lou-parslow <lou.parslow@gmail.com>
2015-09-17 08:10:56 -0600

    'all'

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,5 @@
+VERSION='0.0.219'
 require 'dev'
-
-VERSION='0.0.218'
 
 CLOBBER.include('lib')
 CLOBBER.include('*.{zip,nupkg}')
@@ -13,11 +12,9 @@ task :increment_version do
 	if(Git.has_changes?)
 		puts "updating VERSION from #{major}.#{minor}.#{build} to #{major}.#{minor}.#{next_build}"
 		Text.replace_in_file('rakefile.rb',"VERSION='#{major}.#{minor}.#{build}'","VERSION='#{major}.#{minor}.#{next_build}'")
-		VERSION="#{major}.#{minor}.#{next_build}"
+		#VERSION="#{major}.#{minor}.#{next_build}"
 	end
 end
-
-
 
 vs_version=MSBuild.get_vs_version('Node.Net.Test.sln')
 #COMMANDS[:build] = ["\"#{MSBuild.get_version(vs_version)}\" Node.Net.Test.sln /nologo /p:Configuration=Release /p:Platform=\"Any CPU\"",
@@ -25,15 +22,15 @@ vs_version=MSBuild.get_vs_version('Node.Net.Test.sln')
 #					"\"#{MSBuild.get_version(vs_version)}\" Node.Net.Libs.sln /nologo /p:Configuration=Release /p:Platform=\"Any CPU\"",
 #				    "\"#{MSBuild.get_version(vs_version)}\" Node.Net.Libs.sln /nologo /p:Configuration=Debug /p:Platform=\"Any CPU\""]		      			
 #COMMANDS[:test] = ["\"#{Test.nunit_console}\" \"#{Rake.application.original_dir}/bin/Net4.6/Release/Node.Net.Test.dll\" /xml:\"bin/Net4.6/Release/Node.Net.Test.dll.TestResults.xml\""]
-task :build => [:increment_version] 
+task :build 
 task :publish => [:build,:test,:commit] do
 	puts "publish #{__FILE__}}"
 	if(__FILE__.include?('/wrk/'))
 		Git.tag File.dirname(__FILE__), VERSION
-		source=FileList.new('Node.Net.sln','Node.Net/**/*.{csproj,snk,cs,json,xaml}',
-							'bin/Net4.6/Release/Node.Net.dll','bin/Net4.6/Release/Node.Net.pdb',
-							'bin/Net4.6/Debug/Node.Net.dll','bin/Net4.6/Debug/Node.Net.pdb')
-		Git.publish "https://github.com/lou-parslow/Node.Net.git" ,File.dirname(__FILE__), source, VERSION
+		#source=FileList.new('Node.Net.sln','Node.Net/**/*.{csproj,snk,cs,json,xaml}',
+		#					'bin/Net4.6/Release/Node.Net.dll','bin/Net4.6/Release/Node.Net.pdb',
+		#					'bin/Net4.6/Debug/Node.Net.dll','bin/Net4.6/Debug/Node.Net.pdb')
+		#Git.publish "https://github.com/lou-parslow/Node.Net.git" ,File.dirname(__FILE__), source, VERSION
 	end
 
 	Text.replace_in_file('Node.Net.nuspec',/<version>(\d+.\d+.\d+)<\/version>/,"<version>#{VERSION}</version>")
