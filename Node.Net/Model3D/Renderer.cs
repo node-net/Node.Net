56ea51a231735052f121c0d473c7abf6c52c2cb5
lou-parslow <lou.parslow@gmail.com>
2016-01-07 14:03:47 -0700

    'all'

--- Node.Net/Model3D/Renderer.cs
+++ Node.Net/Model3D/Renderer.cs
@@ -13,7 +13,7 @@
                 if(object.ReferenceEquals(null,viewport3DRenderer))
                 {
                     viewport3DRenderer = new Rendering.Viewport3DRenderer(this);
-                    viewport3DRenderer.ModelVisual3DRenderer.Model3DRenderer.Model3DRequested += Model3DRenderer_Model3DRequested;
+                    viewport3DRenderer.Visual3DRenderer.Model3DRenderer.Model3DRequested += Model3DRenderer_Model3DRequested;
                 }
                 return viewport3DRenderer;
             }
@@ -21,8 +21,9 @@
 
         public virtual System.Windows.Controls.Viewport3D GetViewport3D(object value) => Viewport3DRenderer.GetViewport3D(value);
 
-        public virtual System.Windows.Media.Media3D.ModelVisual3D GetModelVisual3D(object value) => Viewport3DRenderer.ModelVisual3DRenderer.GetModelVisual3D(value);
-        public virtual System.Windows.Media.Media3D.Model3D GetModel3D(object value) => Viewport3DRenderer.ModelVisual3DRenderer.Model3DRenderer.GetModel3D(value);
+        public virtual System.Windows.Media.Media3D.Visual3D GetVisual3D(object value) => Viewport3DRenderer.Visual3DRenderer.GetVisual3D(value);
+        //public virtual System.Windows.Media.Media3D.ModelVisual3D GetModelVisual3D(object value) => Viewport3DRenderer.Visual3DRenderer.GetVisual3D(value);
+        public virtual System.Windows.Media.Media3D.Model3D GetModel3D(object value) => Viewport3DRenderer.Visual3DRenderer.Model3DRenderer.GetModel3D(value);
 
         public event Model3DRequestedEventHandler Model3DRequested;
         System.Windows.Media.Media3D.Model3D Model3DRenderer_Model3DRequested(object value, Measurement.LengthUnit units)
@@ -35,9 +36,9 @@
             return null;
         }
 
-        public System.Windows.Media.Media3D.MeshGeometry3D GetMeshGeometry3D(string name) => Viewport3DRenderer.ModelVisual3DRenderer.Model3DRenderer.GeometryModel3DRenderer.MeshGeometry3DRenderer.GetMeshGeometry3D(name);
+        public System.Windows.Media.Media3D.MeshGeometry3D GetMeshGeometry3D(string name) => Viewport3DRenderer.Visual3DRenderer.Model3DRenderer.GeometryModel3DRenderer.MeshGeometry3DRenderer.GetMeshGeometry3D(name);
         public System.Windows.Media.Media3D.MatrixTransform3D GetMatrixTransform3D_NoScale(System.Collections.IDictionary value,
-                                     Node.Net.Measurement.LengthUnit units = Node.Net.Measurement.LengthUnit.Meter) => Viewport3DRenderer.ModelVisual3DRenderer.Model3DRenderer.MatrixTransform3DRenderer.GetMatrixTransform3D_NoScale(value, units);
-        public System.Windows.Media.Media3D.Material GetMaterial(string name) => Viewport3DRenderer.ModelVisual3DRenderer.Model3DRenderer.GeometryModel3DRenderer.MaterialRenderer.GetMaterial(name);
+                                     Node.Net.Measurement.LengthUnit units = Node.Net.Measurement.LengthUnit.Meter) => Viewport3DRenderer.Visual3DRenderer.Model3DRenderer.MatrixTransform3DRenderer.GetMatrixTransform3D_NoScale(value, units);
+        public System.Windows.Media.Media3D.Material GetMaterial(string name) => Viewport3DRenderer.Visual3DRenderer.Model3DRenderer.GeometryModel3DRenderer.MaterialRenderer.GetMaterial(name);
     }
 }
--- Node.Net/Model3D/Rendering/Visual3DCollectionRenderer.cs
+++ Node.Net/Model3D/Rendering/Visual3DCollectionRenderer.cs
@@ -1,38 +1,34 @@
-﻿namespace Node.Net.Model3D.Rendering
+﻿using System.Collections.Generic;
+using System.Windows.Media.Media3D;
+
+namespace Node.Net.Model3D.Rendering
 {
     class Visual3DCollectionRenderer : Base
     {
         public Visual3DCollectionRenderer(Node.Net.Model3D.IRenderer renderer) : base(renderer) { }
-        private ModelVisual3DRenderer modelVisual3DRenderer = null;//new ModelVisual3DRenderer();
-        public ModelVisual3DRenderer ModelVisual3DRenderer
+        private Visual3DRenderer visual3DRenderer = null;
+        public Visual3DRenderer Visual3DRenderer
         {
             get {
-                if(object.ReferenceEquals(null,modelVisual3DRenderer))
+                if(object.ReferenceEquals(null,visual3DRenderer))
                 {
-                    modelVisual3DRenderer = new ModelVisual3DRenderer(Renderer);
+                    visual3DRenderer = new Visual3DRenderer(Renderer);
                 }
-                return modelVisual3DRenderer; 
+                return visual3DRenderer; 
             }
-            set { modelVisual3DRenderer = value; }
+            set { visual3DRenderer = value; }
         }
-        /*
-        protected override void SetResourceDictionary(System.Windows.ResourceDictionary resources)
-        {
-            base.SetResourceDictionary(resources);
-            modelVisual3DRenderer.ResourceDictionary = resources;
-        }*/
 
-        public System.Windows.Media.Media3D.Visual3D[] GetVisual3DCollection(object value,
+        public Visual3D[] GetVisual3DCollection(object value,
         Node.Net.Measurement.LengthUnit units = Node.Net.Measurement.LengthUnit.Meter) => GetVisual3DCollection(value as System.Collections.IDictionary, units);
-        protected virtual System.Windows.Media.Media3D.Visual3D[] GetVisual3DCollection(System.Collections.IDictionary value,
+        protected virtual Visual3D[] GetVisual3DCollection(System.Collections.IDictionary value,
                     Node.Net.Measurement.LengthUnit units = Node.Net.Measurement.LengthUnit.Meter)
         {
             if (object.ReferenceEquals(null, value)) return null;
-            System.Collections.Generic.List<System.Windows.Media.Media3D.Visual3D> results
-                = new System.Collections.Generic.List<System.Windows.Media.Media3D.Visual3D>();
+            List<Visual3D> results = new List<Visual3D>();
             foreach(object key in value)
             {
-                System.Windows.Media.Media3D.Visual3D visual3D = modelVisual3DRenderer.GetModelVisual3D(value[key], units);
+                Visual3D visual3D = visual3DRenderer.GetVisual3D(value[key], units);
                 if(!object.ReferenceEquals(null,visual3D))
                 {
                     results.Add(visual3D);
