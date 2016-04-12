909bedbeb562374f976a1828e65e29650e9373e9
lou-parslow <lou.parslow@gmail.com>
2016-04-12 11:37:22 -0600

    '0.0.296'

---
+++ Node.Net/Extensions/IDictionary.Extension.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+namespace Node.Net.Extensions
+{
+    public class IDictionaryExtension
+    {
+        public static IDictionary Find(IDictionary source, string key, string value)
+        {
+            if (object.ReferenceEquals(null, source)) return null;
+            if (source.Contains(key) && source[key].ToString() == value)
+            {
+                return source;
+            }
+            foreach (string child_key in source.Keys)
+            {
+                IDictionary childDictionary = source[child_key] as IDictionary;
+                if (!object.ReferenceEquals(null, childDictionary))
+                {
+                    IDictionary result = Find(childDictionary, key, value);
+                    if (!object.ReferenceEquals(null, result)) return result;
+                }
+            }
+            return null;
+        }
+
+    }
+}
