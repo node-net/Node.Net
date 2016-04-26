844781d6c2590b17e28bcff44989dd655325cc7a
Lou Parslow <lou.parslow@gmail.com>
2016-04-26 14:16:32 -0600

    '1.0.2'

---
+++ Node.Net/IWriter.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace Node.Net
+{
+    public interface IWriter
+    {
+        void Save(Stream destination, object value);
+    }
+}
---
+++ Node.Net/Repository.cs
@@ -0,0 +1,12 @@
+﻿namespace Node.Net
+{
+    public class Repository
+    {
+        private static IRepository _default = null;
+        public static IRepository Default
+        {
+            get { return _default; }
+            set { _default = value; }
+        }
+    }
+}
