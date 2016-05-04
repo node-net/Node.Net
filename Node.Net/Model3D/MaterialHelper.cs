bec9eb9fe3cb4bf3fef74b4e759eb6e34e703589
Lou Parslow <lou.parslow@gmail.com>
2016-05-04 09:38:49 -0600

    '1.0.5'

---
+++ Node.Net/Model3D/MaterialHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace Node.Net.Model3D
+{
+    public class MaterialHelper
+    {
+        public static Material GetImageMaterial(ImageSource imageSource)
+        {
+            MaterialGroup material = new MaterialGroup();
+            DiffuseMaterial diffuse = new DiffuseMaterial()
+            {
+                Brush = new ImageBrush()
+                {
+                    ImageSource = imageSource,
+                    TileMode = TileMode.Tile
+                }
+            };
+            material.Children.Add(diffuse);
+            return material;
+        }
+    }
+}
