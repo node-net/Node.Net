4eff9e5bdd8429f241d57223d40e86d52a0bbff9
Lou Parslow <lou.parslow@gmail.com>
2016-05-23 11:53:36 -0600

    '1.0.14'

--- Node.Net/Collections/MetaData.cs
+++ Node.Net/Collections/MetaData.cs
@@ -4,5 +4,26 @@
     {
         private static MetaData _default = new MetaData();
         public static MetaData Default => _default;
+
+        /*
+        public Dictionary<object, Dictionary<string, dynamic>> MetaData = new Dictionary<object, Dictionary<string, dynamic>>();
+        public dynamic GetMetaData(object item, string key)
+        {
+            if (MetaData.ContainsKey(item) && MetaData[item].ContainsKey(key))
+            {
+                return MetaData[item][key];
+            }
+            return null;
+        }
+
+        public void SetMetaData(object item, string key, object value)
+        {
+            if (!MetaData.ContainsKey(item))
+            {
+                MetaData.Add(item, new Dictionary<string, dynamic>());
+            }
+            MetaData[item][key] = value;
+        }
+        */
     }
 }
