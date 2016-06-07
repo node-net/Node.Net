6627602fec5e3fbb2398086f55b34900807ecad8
lou-parslow <lou.parslow@gmail.com>
2016-06-07 15:25:20 -0600

    '1.0.24'

--- Node.Net/Model3D/Transform3DHelper.cs
+++ Node.Net/Model3D/Transform3DHelper.cs
@@ -1,26 +1,58 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
+using System.Windows.Media.Media3D;
 
 namespace Node.Net.Model3D
 {
     public class Transform3DHelper
     {
-        private Collections.MetaData _metaData = new Collections.MetaData();
         public void Clear() { _metaData.Clear(); }
         public void Traverse(object value)
         {
             Traverse(value as IDictionary);
         }
 
+        public Transform3D GetTransform3D(object value)
+        {
+            return MetaData.GetMetaData(value, "Transform3D") as Transform3D;
+        }
         private void Traverse(IDictionary dictionary)
         {
-            if(!object.ReferenceEquals(null,dictionary))
+            if (!object.ReferenceEquals(null, dictionary))
             {
+                SetTransformMetaData(dictionary);
+            }
+        }
 
+        private Collections.MetaData _metaData = new Collections.MetaData();
+        private Collections.MetaData MetaData
+        {
+            get { return _metaData; }
+        }
+        private void SetTransformMetaData(IDictionary dictionary)
+        {
+            if (object.ReferenceEquals(null, dictionary)) return;
+
+            Node.Net.Model3D.Transform3D transform3D = new Node.Net.Model3D.Transform3D();
+            Vector3D translation = Node.Net.Model3D.RenderHelper.GetTranslationMeters(dictionary);
+            transform3D.Translation = new Point3D(translation.X, translation.Y, translation.Z);
+            transform3D.RotationOTS = Node.Net.Model3D.RenderHelper.GetRotationOTSDegrees(dictionary as IDictionary);
+
+            MetaData.SetMetaData(dictionary, "Transform3D", transform3D);
+
+            object parent = MetaData.GetMetaData(dictionary, "Parent");
+            if (!object.ReferenceEquals(null, parent))
+            {
+                Node.Net.Model3D.Transform3D parentTransform = MetaData.GetMetaData(parent, "Transform3D") as Node.Net.Model3D.Transform3D;
+                transform3D.Parent = parentTransform;
+            }
+
+            foreach (string key in dictionary.Keys)
+            {
+                IDictionary childDictionary = dictionary[key] as IDictionary;
+                if (!object.ReferenceEquals(null, childDictionary))
+                {
+                    SetTransformMetaData(childDictionary);
+                }
             }
         }
     }
