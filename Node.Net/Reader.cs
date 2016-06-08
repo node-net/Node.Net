710c648651c544d1ea9eed439c2021b390e4d267
lou-parslow <lou.parslow@gmail.com>
2016-06-08 10:33:45 -0600

    '1.0.24'

--- Node.Net/Reader.cs
+++ Node.Net/Reader.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Node.Net
+﻿namespace Node.Net
 {
     public class Reader
     {
 
         public static IReader Default { get; set; }
-
-        
     }
 }
