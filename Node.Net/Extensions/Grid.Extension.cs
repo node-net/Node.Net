f31bea9eed199bbb8459c25d1a4473dbd35641b1
Lou Parslow <lou.parslow@gmail.com>
2016-06-17 10:22:47 -0600

    '1.0.56'

---
+++ Node.Net/Extensions/Grid.Extension.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Node.Net.Extensions
+{
+    public static class GridExtension
+    {
+        public static void AddRow(Grid grid, string[] content, Brush backgroundBrush = null, Brush foregroundBrush = null)
+        {
+            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+
+            var column = 0;
+            foreach (string value in content)
+            {
+                var label = new Label { Content = value };
+                if (backgroundBrush != null) label.Background = backgroundBrush;
+                if (foregroundBrush != null) label.Foreground = foregroundBrush;
+
+                grid.Children.Add(label);
+                Grid.SetRow(label, grid.RowDefinitions.Count - 1);
+                if (grid.ColumnDefinitions.Count < column + 1) grid.ColumnDefinitions.Add(new ColumnDefinition());
+                Grid.SetColumn(label, column);
+                ++column;
+            }
+        }
+    }
+}
