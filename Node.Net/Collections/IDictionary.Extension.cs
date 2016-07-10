f4c336be518f854c0af70801ea0eaca59c184899
Lou Parslow <lou.parslow@gmail.com>
2016-07-10 17:44:06 -0600

    '1.0.77'

---
+++ Node.Net/Collections/IDictionary.Extension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace Node.Net.Collections
+{
+    public static class IDictionaryExtension
+    {
+        public static T Get<T>(IDictionary dictionary, string name)
+        {
+            if (dictionary.Contains(name))
+            {
+                var value = dictionary[name];
+                if (value != null)
+                {
+                    if (dictionary[name] != null && typeof(T).IsAssignableFrom(value.GetType()))
+                    {
+                        return (T)dictionary[name];
+                    }
+                    if (typeof(T) == typeof(DateTime) && value.GetType()== typeof(string))
+                    {
+                        return (T)((object)DateTime.Parse(value.ToString()));
+                    }
+                }
+            }
+            return default(T);
+        }
+
+        public static void Set(IDictionary dictionary,string key,object value)
+        {
+            if (value == null) dictionary[key] = value;
+            else
+            {
+                if (value.GetType() == typeof(DateTime))
+                {
+                    dictionary[key] = ((DateTime)value).ToString("o");
+                }
+                else
+                {
+                    dictionary[key] = value;
+                }
+            }
+        }
+    }
+}
