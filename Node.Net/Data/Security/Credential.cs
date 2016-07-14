71792b88c5d707e320ef4f6c830c2d84ee57fdb9
lou-parslow <lou.parslow@gmail.com>
2016-07-14 14:27:19 -0600

    '1.0.80'

---
+++ Node.Net/Data/Security/Credential.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Security;
+
+namespace Node.Net.Data.Security
+{
+    public class Credential : Dictionary<string, string>, ICredential
+    {
+        public Credential() { }
+        public Credential(IProtection protection) { _protection = protection; }
+        private IProtection _protection = new CurrentUserProtection();
+        public IProtection Protection
+        {
+            get
+            {
+                return _protection;
+            }
+        }
+        public string Domain
+        {
+            get
+            {
+                if (ContainsKey(nameof(Domain))) return this[nameof(Domain)].ToString();
+                return string.Empty;
+            }
+            set
+            {
+                this[nameof(Domain)] = value;
+            }
+        }
+
+        public string UserName
+        {
+            get
+            {
+                if (ContainsKey(nameof(UserName))) return this[nameof(UserName)].ToString();
+                return string.Empty;
+            }
+            set
+            {
+                this[nameof(UserName)] = value;
+            }
+        }
+
+        public SecureString Password
+        {
+            get
+            {
+                if (ContainsKey(nameof(Password)))
+                {
+                    return Protection.Unprotect(this[nameof(Password)].ToString());
+                }
+
+                return new SecureString();
+            }
+            set
+            {
+                this[nameof(Password)] = Protection.Protect(value);
+            }
+        }
+
+        public static System.Security.SecureString MakeSecureString(string secret)
+        {
+            var secure = new SecureString();
+            System.Array.ForEach(secret.ToCharArray(), secure.AppendChar);
+            secure.MakeReadOnly();
+            return secure;
+        }
+
+    }
+}
---
+++ Node.Net/Data/Security/IProtection.Extension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Security;
+using System.Text;
+
+namespace Node.Net.Data.Security
+{
+    static class IProtectionExtension
+    {
+        public static string Protect(IProtection protection, SecureString secure_secret)
+        {
+            var credential = new NetworkCredential("temp", secure_secret);
+            var bytes = Encoding.UTF8.GetBytes(credential.Password);
+            var secret_bytes = protection.Protect(bytes);
+            return Convert.ToBase64String(secret_bytes);
+
+        }
+
+        public static SecureString Unprotect(IProtection protection, string encrypted_secret)
+        {
+            var secret_bytes = Convert.FromBase64String(encrypted_secret);
+            var bytes = protection.Unprotect(secret_bytes);
+            return Credential.MakeSecureString(Encoding.UTF8.GetString(bytes));
+        }
+    }
+}
