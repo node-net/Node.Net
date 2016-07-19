7cc1b5453e9d70d10ba0882e0d338bded96d97d7
Lou Parslow <lou.parslow@gmail.com>
2016-07-19 10:52:51 -0600

    '1.0.86'

--- Node.Net/Data/Extension/Regex.Extension.cs
+++ Node.Net/Data/Extension/Regex.Extension.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace Node.Net.Data.Extension
@@ -12,8 +7,8 @@ namespace Node.Net.Data.Extension
     {
         public static string[] GetGroupMatches(Regex regex, string text, int group_index = 0)
         {
-            List<string> results = new List<string>();
-            MatchCollection matches = regex.Matches(text);
+            var results = new List<string>();
+            var matches = regex.Matches(text);
             for (int i = 0; i < matches.Count; ++i)
             {
                 if (matches[i].Groups.Count > group_index + 1)
@@ -21,13 +16,6 @@ namespace Node.Net.Data.Extension
                     results.Add(matches[i].Groups[group_index + 1].Value);
                 }
             }
-            /*
-            Assert.AreEqual(2, matches.Count);
-            GroupCollection groups0 = matches[0].Groups;
-            GroupCollection groups1 = matches[1].Groups;
-            Assert.AreEqual("rakefile", matches[0].Groups[1].Value);
-            Assert.AreEqual("test", matches[1].Groups[1].Value);*/
-
             return results.ToArray();
         }
     }
