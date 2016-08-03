3cc056102538b07953ac487954fcfb022f8d62ff
lou-parslow <lou.parslow@gmail.com>
2016-08-03 12:28:43 -0600

    '1.0.96'

---
+++ Node.Net/Collections/Interfaces.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Node.Net.Collections
+{
+    public interface IChild { IParent Parent { get; set; } }
+    public interface IParent { Dictionary<string, IChild> GetChildren(); }
+}
