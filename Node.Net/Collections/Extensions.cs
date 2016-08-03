90757ee414937c1ed51ea0ba71e8ccb7b2df36ef
Lou Parslow <lou.parslow@gmail.com>
2016-08-03 17:53:38 -0600

    '1.0.97'

--- Node.Net/Collections/Extensions.cs
+++ Node.Net/Collections/Extensions.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 
 namespace Node.Net.Collections
 {
@@ -7,7 +6,5 @@ namespace Node.Net.Collections
     {
         public static T Get<T>(this IDictionary dictionary, string name) => IDictionaryExtension.Get<T>(dictionary, name);
         public static void Set(this IDictionary dictionary, string name, object value) => IDictionaryExtension.Set(dictionary, name, value);
-        public static Dictionary<string, T> Collect<T>(this IDictionary dictionary) => IDictionaryExtension.Collect<T>(dictionary);
-        public static string[] CollectUniqueStrings(this IDictionary dictionary, string key) => IDictionaryExtension.CollectUniqueStrings(dictionary, key);
     }
 }
