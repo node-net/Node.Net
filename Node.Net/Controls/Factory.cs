504b0e7024b7ea785fc9b128c464a66f404a4d65
Lou Parslow <lou.parslow@gmail.com>
2016-08-03 18:53:00 -0600

    '1.0.98'

---
+++ Node.Net/Controls/Factory.cs
@@ -0,0 +1,23 @@
+﻿namespace Node.Net.Controls
+{
+    public class Factory
+    {
+        private static Factory _default;
+        public static Factory Default
+        {
+            get
+            {
+                if (_default == null)
+                {
+                    _default = new Factory();
+                }
+                return _default;
+            }
+        }
+
+        public T Create<T>(object value)
+        {
+            return default(T);
+        }
+    }
+}
