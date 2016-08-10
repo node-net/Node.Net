47e21c367244e6d288416b0d0aee2ce77f89dd94
Lou Parslow <lou.parslow@gmail.com>
2016-08-10 16:25:18 -0600

    '1.0.101'

---
+++ Node.Net/Factory/Internal/Matrix3DFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Node.Net.Factory.Internal
+{
+    class Matrix3DFactory : IFactory
+    {
+        public T Create<T>(object value)
+        {
+            return default(T);
+        }
+    }
+}
