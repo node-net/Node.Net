815c96ad70bd78e2a40bcbd61a4c3432a0884d4c
lou-parslow <lou.parslow@gmail.com>
2016-08-15 15:02:52 -0600

    '1.0.103'

---
+++ Node.Net/Factory/Extension/Visual.Extension.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace Node.Net.Factory.Extension
+{
+    static class VisualExtension
+    {
+        public static Point3D? HitTest(Visual visual, double x, double y)
+        {
+            var result = VisualTreeHelper.HitTest(visual, new Point(x, y)) as RayMeshGeometry3DHitTestResult;
+            return result?.PointHit;
+        }
+    }
+}
--- Node.Net/Factory/Extensions.cs
+++ Node.Net/Factory/Extensions.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
 
 namespace Node.Net.Factory
 {
     public static class Extensions
     {
         public static T Create<T>(this IFactory factory, object value) => Internal.IFactoryExtension.Create<T>(factory, value);
+        public static Point3D? HitTest(this Visual3D reference, Point3D point, Vector3D direction) => Extension.Visual3DExtension.HitTest(reference, point, direction);
     }
 }
