03a3e4ee71f561d298e2170fdeaba221c6673783
Lou Parslow <lou.parslow@gmail.com>
2016-08-15 21:43:05 -0600

    '1.0.106'

---
+++ Node.Net/Factory/Factories/IFactoryAdapter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Node.Net.Factory.Factories
+{
+    class IFactoryAdapter : IFactory
+    {
+        Func<Type, object, object> CreateFunction { get; set; }
+        public object Create(Type targetType, object instance)
+        {
+            if (CreateFunction != null) return CreateFunction(targetType, instance);
+            return null;
+        }
+    }
+}
