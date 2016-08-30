6d5d97dcdc70fd484d863708fbe0ab7d3726cac2
Lou Parslow <lou.parslow@gmail.com>
2016-08-29 18:13:51 -0600

    '1.0.122'

--- Node.Net/Factory.cs
+++ Node.Net/Factory.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +11,16 @@ namespace Node.Net
     // TODO: rename Node.Net.Factory to Node.Net.Factories
     public class Factory : IFactory
     {
+        public Factory() { }
+        public Factory(Assembly assembly)
+        {
+            factory.ResourceAssemblies.Add(assembly);
+        }
+
+        private Node.Net.Factories.Factory factory = new Factories.Factory();
         public object Create(Type targetType,object source)
         {
-            return null;
+            return factory.Create(targetType,source);
         }
     }
 }
