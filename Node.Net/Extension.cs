c917987bbf13dfda1cbee4e292571bd06b1c0ef4
lou-parslow <lou.parslow@gmail.com>
2016-10-26 13:26:13 -0600

    '1.0.186'

--- Node.Net/Extension.cs
+++ Node.Net/Extension.cs
@@ -63,6 +63,7 @@ namespace Node.Net
 
         #region Object
         public static object GetKey(this object item) => Collections.ObjectExtension.GetKey(item);
+        public static object GetValue(this object item) => Collections.ObjectExtension.GetValue(item);
         public static object GetParent(this object item) => Collections.ObjectExtension.GetParent(item);
         public static void SetParent(this object item, object parent) => Collections.ObjectExtension.SetParent(item, parent);
         public static T GetNearestAncestor<T>(this object child) => Collections.ObjectExtension.GetNearestAncestor<T>(child);
