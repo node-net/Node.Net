5abf87f55860fbfeeb4b10ce3a0d8aaa9e5823c7
lou-parslow <lou.parslow@gmail.com>
2016-11-15 09:41:02 -0700

    '1.0.216'

--- Node.Net/Filters.cs
+++ Node.Net/Filters.cs
@@ -12,5 +12,11 @@ namespace Node.Net
         public static Func<object, bool?> GetValueFilter<T>(T value, bool? default_result = false) => Collections.Filters.GetValueFilter<T>(value, default_result);
         public static Func<object, bool?> GetStringFilter(string value, string pattern = null, bool? default_result = false) => Collections.Filters.GetStringFilter(value, pattern, default_result);
         public static Func<object, bool?> GetIDictionaryFilter<T>(string key, string keyStringValue, string keyStringPattern = null, bool? default_result = false) where T : IDictionary => Collections.Filters.GetIDictionaryFilter<T>(key, keyStringValue, keyStringPattern, default_result);
+        public static Func<object, bool?> GetTypeFilter<T>(string typename,bool exact) where T : IDictionary
+        {
+            if (exact) return GetIDictionaryFilter<T>("Type", typename);
+            else return GetIDictionaryFilter<T>("Type", null, typename);
+        }
+        public static Func<object, bool?> GetTypeFilter(string typename, bool exact = true) => GetTypeFilter<IDictionary>(typename, exact);
     }
 }
