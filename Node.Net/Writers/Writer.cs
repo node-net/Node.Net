dacd47439988a998b9ee97c58d5f2cac317f975b
Lou Parslow <lou.parslow@gmail.com>
2016-11-15 18:03:25 -0700

    '1.0.223'

---
+++ Node.Net/Writers/Writer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Node.Net.Writers
+{
+    public class Writer
+    {
+        public static Writer Default { get; } = new Writer();
+        public void Write(Stream stream, object value)
+        {
+
+        }
+    }
+}
