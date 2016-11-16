263bf3137f0000ade4d74a632d511130282f2e3c
lou-parslow <lou.parslow@gmail.com>
2016-11-16 13:43:02 -0700

    '1.0.224'

---
+++ Node.Net/Repositories/DirectoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Node.Net.Repositories
+{
+    public class DirectoryRepository
+    {
+        public string Directory { get; set; }
+        public Func<Stream, object> ReadFunction { get; set; } = Repository.DefaultReadFunction;
+        public Action<Stream, object> WriteFunction { get; set; } = Repository.DefaultWriteFunction;
+        public object Get(string name)
+        {
+            if (!System.IO.Directory.Exists(Directory)) return null;
+            if (ReadFunction == null) return null;
+            var filename = $"{Directory}\\{name}";
+            if (File.Exists(filename))
+            {
+                using (FileStream fs = new FileStream(filename, FileMode.Open))
+                {
+                    return ReadFunction(fs);
+                }
+            }
+            return null;
+        }
+        public void Set(string name, object value)
+        {
+            if (System.IO.Directory.Exists(Directory))
+            {
+                if (WriteFunction != null)
+                {
+                    var filename = $"{Directory}\\{name}";
+                    using (FileStream fs = new FileStream(filename, FileMode.Create))
+                    {
+                        WriteFunction(fs, value);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Node.Net/Repositories/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Markup;
+
+namespace Node.Net.Repositories
+{
+    public class Repository
+    {
+        public static Func<Stream, object> DefaultReadFunction { get; set; } = XamlReader.Load;
+        public static Action<Stream, object> DefaultWriteFunction { get; set; } = Write;
+        private static void Write(Stream stream, object value) => XamlWriter.Save(value, stream);
+        public Func<string, object> AbsoluteGetFunction { get; set; } = new UriRepository().Get;
+        //public Func<string, object> AbsoluteSetFunction { get; set; } = new UriRepository().Set;
+        public Dictionary<string,Func<string,object>> RelativeGetFunctions { get; set; }
+    }
+}
