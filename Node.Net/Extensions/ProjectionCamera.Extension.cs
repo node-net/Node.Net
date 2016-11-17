68d07293fd9bbb2244cf61ae871255399258b6fb
lou-parslow <lou.parslow@gmail.com>
2016-11-17 11:35:58 -0700

    '1.0.224'

---
+++ Node.Net/Extensions/ProjectionCamera.Extension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace Node.Net.Extensions
+{
+    public static class ProjectionCameraExtension
+    {
+        public static void SetDirection(ProjectionCamera target, ProjectionCamera source)
+        {
+            target.LookDirection = source.LookDirection;
+            target.UpDirection = source.UpDirection;
+        }
+
+    }
+}
