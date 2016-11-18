ca05d2c5b4bee04056c892d409532e122791758e
Lou Parslow <lou.parslow@gmail.com>
2016-11-17 19:20:52 -0700

    '1.0.226'

---
+++ Node.Net/Extensions/Assembly.Extension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Node.Net.Extensions
+{
+    public static class AssemblyExtension
+    {
+        public static Stream GetStream(this Assembly assembly, string name)
+        {
+            foreach (var resourceName in assembly.GetManifestResourceNames())
+            {
+                if (resourceName.Contains(name)) return assembly.GetManifestResourceStream(resourceName);
+            }
+            return null;
+        }
+    }
+}
