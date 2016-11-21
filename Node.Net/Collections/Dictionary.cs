e3d18996c4fdb11844c14f4f12828c8210cdce1c
Lou Parslow <lou.parslow@gmail.com>
2016-11-21 15:52:58 -0700

    '1.0.231'

---
+++ Node.Net/Collections/Dictionary.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Node.Net.Collections
+{
+    public sealed class Dictionary : Dictionary<string, dynamic>
+    {
+        public Dictionary() { }
+        public Dictionary(string type) { Add("Type", type); }
+        public string Type
+        {
+            get
+            {
+                var value = IDictionaryExtension.Get<string>(this, "Type");
+                return (value == null) ? string.Empty : value.ToString();
+            }
+        }
+        public string Key
+        {
+            get
+            {
+                var key = ObjectExtension.GetKey(this);
+                return (key == null) ? string.Empty : key.ToString();
+            }
+        }
+        public string FullKey => ObjectExtension.GetFullKey(this);
+        public string[] Types => IDictionaryExtension.CollectValues<string>(this, "Type");
+        public void UpdateParentReferences() => IDictionaryExtension.DeepUpdateParents(this);
+    }
+}
--- Node.Net/Extensions/ProjectionCamera.Extension.cs
+++ Node.Net/Extensions/ProjectionCamera.Extension.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows.Media.Media3D;
 
 namespace Node.Net.Extensions
@@ -15,5 +11,79 @@ namespace Node.Net.Extensions
             target.UpDirection = source.UpDirection;
         }
 
+        private static Dictionary<string, ProjectionCamera> namedCameras;
+        public static Dictionary<string, ProjectionCamera> NamedCameras
+        {
+            get
+            {
+                if (namedCameras == null)
+                {
+                    namedCameras = new Dictionary<string, ProjectionCamera>();
+                    namedCameras.Add("Top",
+                        new PerspectiveCamera
+                        {
+                            LookDirection = new Vector3D(0, 0, 1),
+                            UpDirection = new Vector3D(0, 1, 0)
+                        });
+                    namedCameras.Add("Bottom",
+                        new PerspectiveCamera
+                        {
+                            LookDirection = new Vector3D(0, 0, 1),
+                            UpDirection = new Vector3D(0, -1, 0)
+                        });
+                    namedCameras.Add("Front",
+                        new PerspectiveCamera
+                        {
+                            LookDirection = new Vector3D(0, 1, 0),
+                            UpDirection = new Vector3D(0, 0, 1)
+                        });
+                    namedCameras.Add("Back",
+                        new PerspectiveCamera
+                        {
+                            LookDirection = new Vector3D(0, -1, 0),
+                            UpDirection = new Vector3D(0, 0, 1)
+                        });
+                    namedCameras.Add("Left",
+                        new PerspectiveCamera
+                        {
+                            LookDirection = new Vector3D(1, 0, 0),
+                            UpDirection = new Vector3D(0, 0, 1)
+                        });
+                    namedCameras.Add("Right",
+                        new PerspectiveCamera
+                        {
+                            LookDirection = new Vector3D(-1, 0, 0),
+                            UpDirection = new Vector3D(0, 0, 1)
+                        });
+                    namedCameras.Add("SW",
+                        new PerspectiveCamera
+                        {
+                            LookDirection = new Vector3D(1, 1, -1),
+                            UpDirection = new Vector3D(1, 1, 1)
+                        });
+                    namedCameras.Add("NW",
+                        new PerspectiveCamera
+                        {
+                            LookDirection = new Vector3D(1, -1, -1),
+                            UpDirection = new Vector3D(1, -1, 1)
+                        });
+                    namedCameras.Add("NE",
+                        new PerspectiveCamera
+                        {
+                            LookDirection = new Vector3D(-1, -1, -1),
+                            UpDirection = new Vector3D(-1, -1, 1)
+                        });
+                    namedCameras.Add("SE",
+                        new PerspectiveCamera
+                        {
+                            LookDirection = new Vector3D(-1, 1, -1),
+                            UpDirection = new Vector3D(-1, 1, 1)
+                        });
+                }
+                return namedCameras;
+
+
+            }
+        }
     }
 }
