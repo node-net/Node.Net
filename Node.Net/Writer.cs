039f35ed48c72e15137f653252763180c1fd6de7
lou-parslow <lou.parslow@gmail.com>
2016-11-28 11:33:20 -0700

    '1.0.237'

--- Node.Net/Writer.cs
+++ Node.Net/Writer.cs
@@ -10,6 +10,7 @@ namespace Node.Net
     {
         public static Writer Default { get; } = new Writer();
         public void Write(Stream stream, object value) { writer.Write(stream, value); }
+        public void Write(string filename,object value){ writer.Write(filename, value); }
         private Node.Net.Writers.Writer writer = new Node.Net.Writers.Writer();
     }
 }
