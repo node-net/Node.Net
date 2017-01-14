790d41cf18e1aacfbf5bc08a9c33f4887b7d266f
Lou Parslow <lou.parslow@gmail.com>
2017-01-14 11:17:03 -0700

    '1.0.267'

---
+++ Node.Net/Extensions/Rect3D.Extension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace Node.Net.Extensions
+{
+    public static class Rect3DExtension
+    {
+        public static Rect3D Scale(this Rect3D source, double factor)
+        {
+            if (source.IsEmpty) return source;
+            var newSize = new Size3D(source.SizeX * factor, source.SizeY * factor, source.SizeZ * factor);
+            return new Rect3D
+            {
+                Size = newSize,
+                Location = new Point3D
+                (
+                    source.X + (source.SizeX - newSize.X) / 2.0,
+                    source.Y + (source.SizeY - newSize.Y) / 2.0,
+                    source.Z + (source.SizeZ - newSize.Z) / 2.0
+                )
+            };
+        }
+    }
+}
