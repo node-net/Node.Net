e52cda609346b735337e34998035b8f2ff01cc54
Lou Parslow <lou.parslow@gmail.com>
2017-01-15 15:33:59 -0700

    '1.0.270'

--- Node.Net/Readers/TextReader.cs
+++ Node.Net/Readers/TextReader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,6 +23,26 @@ namespace Node.Net.Readers
                 }
             }
             return text;
+        }
+        public object Read(string filename) { return IReadExtension.Read(this, filename); }
+        public object Read(Type type, string name) { return IReadExtension.Read(this, type, name); }
+        public object Read(Assembly assembly, string name) { return IReadExtension.Read(this, assembly, name); }
+        public object Open(string openFileDialogFilter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*")
+        {
+            var ofd = new Microsoft.Win32.OpenFileDialog { Filter = openFileDialogFilter };
+            var result = ofd.ShowDialog();
+            if (result == true)
+            {
+                try
+                {
+                    return Read(ofd.FileName);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Unable to open file {ofd.FileName}", ex);
+                }
+            }
+            return null;
         }
     }
 }
