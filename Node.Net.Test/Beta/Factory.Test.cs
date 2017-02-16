0b266784d8a932e35611cc7723635815826c2338
Lou Parslow <lou.parslow@gmail.com>
2017-02-16 16:36:01 -0700

    'all'

---
+++ Node.Net.Test/Beta/Factory.Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Node.Net.Tests.Beta
+{
+    class Factory
+    {
+    }
+}
