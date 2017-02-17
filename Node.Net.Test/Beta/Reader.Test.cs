6c9cc67b1a4dcd2b2372351eccab1d6c71fe8986
Lou Parslow <lou.parslow@gmail.com>
2017-02-17 13:37:52 -0700

    'all'

--- Node.Net.Test/Beta/Reader.Test.cs
+++ Node.Net.Test/Beta/Reader.Test.cs
@@ -1,14 +1,37 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
 
 namespace Node.Net.Beta
 {
-    [TestFixture]
+    [TestFixture,Category("Beta")]
     class ReaderTest
     {
+        [Test]
+        public void Reader_Read()
+        {
+            var data = new Dictionary<string, Type>
+            {
+                {"Widget.MeshGeometry3D.xaml" , typeof(MeshGeometry3D) }
+            };
+            var factory = new Factory();
+            factory.ManifestResourceAssemblies.Add(typeof(ReaderTest).Assembly);
+            var reader = new Reader();
+            foreach(var name in data.Keys)
+            {
+                var stream = factory.Create<Stream>(name);
+                Assert.NotNull(stream, $"null stream for '{name}'");
+
+                var instance = reader.Read(stream);
+                //Assert.NotNull(instance, $"null instance for '{name}'");
+
+                //Assert.True(data[name].IsAssignableFrom(instance.GetType()), $"type {data[name].FullName} not assignable from instance");
+            }
+        }
     }
 }
--- rakefile.rb
+++ rakefile.rb
@@ -1,4 +1,4 @@
-VERSION='1.0.320'
+VERSION='1.0.321'
 require 'dev'
 
 task :publish  do
