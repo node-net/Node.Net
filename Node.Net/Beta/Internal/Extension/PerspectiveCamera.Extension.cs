115f507184c08b800b7d15322fcc4e558183e72f
lou-parslow <lou.parslow@gmail.com>
2017-04-18 08:49:44 -0600

    'all'

--- Node.Net/Beta/Internal/Extension/PerspectiveCamera.Extension.cs
+++ Node.Net/Beta/Internal/Extension/PerspectiveCamera.Extension.cs
@@ -22,6 +22,19 @@ namespace Node.Net.Beta.Internal
             if (Abs(local.Y) > frustrumHeight / 2.0) return false;
             return true;
         }
+        public static bool IsVisible(this PerspectiveCamera camera, Point3D worldPoint,double horizontalFOV,double verticalFOV)
+        {
+            var local = ProjectionCameraExtension.GetWorldToLocal(camera).Transform(worldPoint);
+            // camera lookdirection is along -Z axis
+            if (local.Z >= 0.0) return false;
+            const double deg2rad = 0.01745329;
+            var distance = Abs(local.Z);
+            var frustrumWidth = 2.0 * distance * Tan(horizontalFOV * deg2rad * 0.5);
+            var frustrumHeight = 2.0 * distance * Tan(verticalFOV * deg2rad * 0.5);
+            if (Abs(local.X) > frustrumWidth / 2.0) return false;
+            if (Abs(local.Y) > frustrumHeight / 2.0) return false;
+            return true;
+        }
         public static PerspectiveCamera GetTransformedPerspectiveCamera(this PerspectiveCamera camera, Transform3D transform)
         {
             return new PerspectiveCamera
--- rakefile.rb
+++ rakefile.rb
@@ -1,4 +1,4 @@
-VERSION='1.0.390'
+VERSION='1.0.391'
 require 'dev'
 
 task :publish  do
