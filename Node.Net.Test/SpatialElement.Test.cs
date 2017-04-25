c835c4b837fa93600db136a2bc25e697165de096
lou-parslow <lou.parslow@gmail.com>
2017-04-25 15:36:18 -0600

    'all'

---
+++ Node.Net.Test/SpatialElement.Test.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Node.Net
+{
+    [TestFixture]
+    class SpatialElementTest
+    {
+        [Test,Apartment(ApartmentState.STA),Explicit]
+        [TestCase("Scene.json")]
+        public void SpatialElement_Visual3D(string name)
+        {
+            var data = Factory.Default.Create<IDictionary>(name);
+            Assert.NotNull(data, nameof(data));
+
+            var factory = new Factory
+            {
+                ReadFunction = new Reader { DefaultObjectType = typeof(SpatialElement) }.Read
+            };
+            //factory.I
+            //var v3d = Factory.Default.
+        }
+    }
+}
