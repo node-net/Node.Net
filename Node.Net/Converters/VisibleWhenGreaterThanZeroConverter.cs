086191658aea8b96782761a37efe08b7c9dd69ef
lou-parslow <lou.parslow@gmail.com>
2018-04-02 09:12:31 -0600

    'all'

---
+++ Node.Net/Converters/VisibleWhenGreaterThanZeroConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Node.Net.Converters
+{
+    public class VisibleWhenGreaterThanZeroConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var dvalue = System.Convert.ToDouble(value);
+            if (dvalue > 0.0) return Visibility.Visible;
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
