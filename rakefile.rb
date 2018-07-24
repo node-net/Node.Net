2da0abcf3fb9c0bc0814141636ac7ea3fb84141e
lou-parslow <lou.parslow@gmail.com>
2018-07-24 12:59:13 -0600

    'all'

--- rakefile.rb
+++ rakefile.rb
@@ -1,13 +1,10 @@
-VERSION='1.1.22'
-#SLN_FILES=FileList.new('Node.Net.NETStandard2.0.sln','Node.Net.NETFramework4.6.sln')
+VERSION='1.2.0'
 require 'dev'
 CLOBBER.include('**/obj','bin','TestResults')
 
 task :publish  do
 	list=`nuget list Node.Net -Source VSTS -Prerelease`
 	if(!list.include?("Node.Net #{VERSION}"))
-	#	# Have VSTS package and push the NuGet Packages
-	#	puts `nuget push Ouroboros.Reflection.#{VERSION}.nupkg -Source "https://lou-parslow.pkgs.visualstudio.com/_packaging/Packages/nuget/v3/index.json" -ApiKey VSTS`
-		puts `nuget push Node.Net.#{VERSION}-beta.nupkg -Source https://api.nuget.org/v3/index.json`
+		puts `nuget push Node.Net.#{VERSION}.nupkg -Source https://api.nuget.org/v3/index.json`
 	end
 end
