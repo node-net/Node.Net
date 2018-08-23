21c7a84bcc15c77e2433cd0c0068745e6d7ddd81
lou-parslow <lou.parslow@gmail.com>
2018-08-23 10:59:01 -0600

    'all'

---
+++ src/Node.Net/Converters/VisibleWhenTrue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Node.Net.Converters
+{
+	public sealed class VisibleWhenTrue : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null) return Visibility.Hidden;
+			var bvalue = System.Convert.ToBoolean(value);
+			if (bvalue) return Visibility.Visible;
+			return Visibility.Hidden;
+		}
+		/// <summary>
+		/// ConvertBack
+		/// </summary>
+		/// <param name="value"></param>
+		/// <param name="targetType"></param>
+		/// <param name="parameter"></param>
+		/// <param name="culture"></param>
+		/// <returns></returns>
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
