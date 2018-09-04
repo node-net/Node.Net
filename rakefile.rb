cbd0cf445044295f781679a49aa11abcc77266b8
lou-parslow <lou.parslow@gmail.com>
2018-09-04 13:56:45 -0600

    'all'

--- rakefile.rb
+++ rakefile.rb
@@ -3,7 +3,7 @@ require 'dev'
 CLOBBER.include('**/obj','bin','TestResults')
 
 task :publish  do
-	list=`nuget list Node.Net -Source VSTS -Prerelease`
+	list=`nuget list Node.Net -Source nuget.org`
 	if(!list.include?("Node.Net #{VERSION}"))
 		puts `nuget push Node.Net.#{VERSION}.nupkg -Source https://api.nuget.org/v3/index.json`
 	end
