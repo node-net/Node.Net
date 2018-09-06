893cbbbacadfa32591c5076504f862d557452294
lou-parslow <lou.parslow@gmail.com>
2018-09-06 09:23:10 -0600

    'all'

---
+++ src/Node.Net/Extension/SerializationInfo.Extension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Node.Net
+{
+	public static class SerializationInfoExtension
+	{
+		public static IDictionary GetPersistentData(this SerializationInfo info)
+		{
+			var data = new Dictionary<string, object>();
+			SerializationInfoEnumerator e = info.GetEnumerator();
+			while (e.MoveNext())
+			{
+				data.Add(e.Name, e.Value);
+			}
+			return data;
+		}
+		public static void SetPersistentData(this SerializationInfo info, IDictionary data)
+		{
+			foreach (string key in data.Keys)
+			{
+				info.AddValue(key,data[key]);
+			}
+		}
+	}
+}
