ccad648da1a4bd69f5444e4547f0e2988fb32b58
lparslow <lou.parslow@gmail.com>
2018-09-07 18:13:16 -0600

    'all'

--- src/Node.Net.Test/JsonRPC/Responder.Test.cs
+++ src/Node.Net.Test/JsonRPC/Responder.Test.cs
@@ -31,5 +31,6 @@ namespace Node.Net.JsonRPC
 		{
 			return "hello";
 		}
+		
 	}
 }
--- src/Node.Net.Test/JsonRPC/Server.Test.cs
+++ src/Node.Net.Test/JsonRPC/Server.Test.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Net;
 using System.Security.Principal;
 
 namespace Node.Net.JsonRPC
@@ -6,23 +7,37 @@ namespace Node.Net.JsonRPC
 	[TestFixture]
 	internal class ServerTest
 	{
-		[Test]
-		public void ServerUsage()
+		public Response Respond(Request request)
 		{
-			var identity = WindowsIdentity.GetCurrent();
-			var principal = new WindowsPrincipal(identity);
-			if (principal.IsInRole(WindowsBuiltInRole.Administrator))
+			if(request.Method == "sayHello")
 			{
-				var machine = System.Environment.MachineName;
-				var url = $"http://{machine}:9999/JsonRPC/";
-				using (var server = new Server(ResponderTest.GetTestResponder().Respond, url))
-				{
-					server.Start();
+				return new Response("hello", request.Id);
+			}
+			return new Response(new Error(-32601,"Method not found"),request.Id);
+		}
+		[Test]
+		public void Default_Usage()
+		{
+			using (var server = new Server(Respond))
+			{
+				var port = server.Port;
+				server.Start();
 
-					var client = new Client(url);
-					var response = client.Respond(new Request("sayHello"));
-					Assert.AreEqual("hello", response.Result.ToString());
+				var uri = server.Uri;
+				Assert.AreEqual($"http://localhost:{port}/", uri.ToString());
+				using (var client = new WebClient())
+				{
+					var request = new Request("sayHello");
+					var json_response = client.UploadString(uri.ToString(), request.ToJson());
+					Assert.NotNull(json_response, nameof(json_response));
+					Assert.True(json_response.Contains("hello"));
+
 				}
+				var jsonRpcClient = new Client(server.Uri.ToString());
+				var response = jsonRpcClient.Respond(new Request("sayHello"));
+				Assert.NotNull(response, nameof(response));
+				Assert.AreEqual("hello", response.Result.ToString());
+				server.Stop();
 			}
 		}
 	}
