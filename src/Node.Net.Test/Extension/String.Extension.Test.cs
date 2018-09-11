10422386cfff6989728a1787b34266889640a960
lou-parslow <lou.parslow@gmail.com>
2018-09-11 08:52:55 -0600

    'all'

--- src/Node.Net.Test/Extension/String.Extension.Test.cs
+++ src/Node.Net.Test/Extension/String.Extension.Test.cs
@@ -15,5 +15,12 @@ namespace Node.Net.Test.Extension
 			text = new StreamReader("Object.Coverage.json".GetStream()).ReadToEnd();
 			Assert.True(text.Contains("array_empty"), "array_empty not found in Object.Coverage.json");
 		}
+		[Test]
+		public void GetRawValue()
+		{
+			Assert.AreEqual(10.0, "10'".GetRawValue());
+			Assert.AreEqual(10.0, "10 ft".GetRawValue());
+			Assert.AreEqual(10.0, "10 m".GetRawValue());
+		}
 	}
 }
