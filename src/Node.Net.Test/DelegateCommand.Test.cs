ddfc4d5d313ae579df210c44631df74a5fbfc6d2
lou-parslow <lou.parslow@gmail.com>
2018-10-04 15:24:42 -0600

    'all'

---
+++ src/Node.Net.Test/DelegateCommand.Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Node.Net.Test
+{
+	[TestFixture]
+	class DelegateCommandTest
+	{
+		[Test]
+		public void Usage()
+		{
+			var command = new DelegateCommand(TestCommand);
+			command.Execute(null);
+		}
+		public void TestCommand(object i) { }
+	}
+}
