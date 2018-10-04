bb0fed2c7c246500863675ce9c146403dbaa3fed
lou-parslow <lou.parslow@gmail.com>
2018-10-04 15:41:37 -0600

    'all'

---
+++ src/Node.Net.Test/Extension/Rect3D.Extension.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+using NUnit.Framework;
+
+namespace Node.Net.Test.Extension
+{
+	[TestFixture]
+	class Rect3DExtensionTest
+	{
+		[Test]
+		public void Usage()
+		{
+			var rect = new Rect3D();
+			rect.GetCenter();
+			rect.Scale(1.1);
+		}
+	}
+}
