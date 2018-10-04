876594eeaab547a032e2adfc8d9c21d6c840a93c
lou-parslow <lou.parslow@gmail.com>
2018-10-04 15:55:04 -0600

    'all'

---
+++ src/Node.Net.Test/Converters/HiddenWhenNull.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Node.Net.Test.Converters
+{
+	[TestFixture]
+	class HiddenWhenNullTest
+	{
+		[Test]
+		public void Usage()
+		{
+			var c = new Node.Net.Converters.HiddenWhenNull();
+			c.Convert(null, typeof(object), null, null);
+			c.Convert(1, typeof(object), null, null);
+			Assert.Throws<NotImplementedException>(() => c.ConvertBack(null, typeof(object), null, null));
+		}
+	}
+}
