e28d269e6f8075f2facf7b27218d81bfa36b3ea2
lou-parslow <lou.parslow@gmail.com>
2018-10-25 14:04:44 -0600

    'all'

--- src/Node.Net/Extension/Request.Extension.cs
+++ src/Node.Net/Extension/Request.Extension.cs
@@ -28,13 +28,50 @@ namespace Node.Net
 					{
 						results.Add(request.Parameters[name]);
 					}
-					else
+				}
+			}
+			if(results.Count == 0)
+			{
+				for(int i = 0; i < names.Length; ++i)
+				{
+					if(request.Parameters.ContainsKey(i.ToString()))
 					{
-						results.Add(null);
+						results.Add(request.Parameters[i.ToString()]);
 					}
 				}
 			}
 			return results.ToArray();
+		}
+
+		public static object[] GetParameterArray<T1>(this JsonRPC.Request request, string[] names)
+		{
+			var parameters = request.GetParameterArray(names);
+			return new object[] { parameters.Get<T1>(0)};
+		}
+		public static object[] GetParameterArray<T1, T2>(this JsonRPC.Request request, string[] names)
+		{
+			var parameters = request.GetParameterArray(names);
+			return new object[] { parameters.Get<T1>(0), parameters.Get<T2>(1) };
+		}
+		public static object[] GetParameterArray<T1,T2,T3>(this JsonRPC.Request request,string[] names)
+		{
+			var parameters = request.GetParameterArray(names);
+			return new object[] { parameters.Get<T1>(0), parameters.Get<T2>(1), parameters.Get<T3>(2) };
+		}
+		public static object[] GetParameterArray<T1, T2, T3,T4>(this JsonRPC.Request request, string[] names)
+		{
+			var parameters = request.GetParameterArray(names);
+			return new object[] { parameters.Get<T1>(0), parameters.Get<T2>(1), parameters.Get<T3>(2),parameters.Get<T4>(3) };
+		}
+		public static object[] GetParameterArray<T1, T2, T3, T4,T5>(this JsonRPC.Request request, string[] names)
+		{
+			var parameters = request.GetParameterArray(names);
+			return new object[] { parameters.Get<T1>(0), parameters.Get<T2>(1), parameters.Get<T3>(2), parameters.Get<T4>(3),parameters.Get<T5>(4) };
+		}
+		public static object[] GetParameterArray<T1, T2, T3, T4, T5,T6>(this JsonRPC.Request request, string[] names)
+		{
+			var parameters = request.GetParameterArray(names);
+			return new object[] { parameters.Get<T1>(0), parameters.Get<T2>(1), parameters.Get<T3>(2), parameters.Get<T4>(3), parameters.Get<T5>(4),parameters.Get<T6>(5) };
 		}
 	}
 }
