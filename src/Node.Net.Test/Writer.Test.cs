7c7c4f4a36b7d67da200b3927a60bc3cd32dc371
lou-parslow <lou.parslow@gmail.com>
2018-11-07 13:27:44 -0700

    'all'

---
+++ src/Node.Net.Test/Writer.Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Node.Net.Test
+{
+	[TestFixture]
+	internal static class WriterTest
+	{
+		[Test]
+		public static void WriteJson()
+		{
+			var data = new Dictionary<string, object>
+			{
+				{"name","test" }
+			};
+
+			var writer = new Writer { JsonFormat = JsonFormat.Compact };
+			using (var memory = new MemoryStream())
+			{
+				writer.Write(memory, data);
+				var json = Encoding.UTF8.GetString(memory.ToArray());
+				Assert.AreEqual("{\"name\":\"test\"}", json);
+			}
+
+			writer = new Writer { JsonFormat = JsonFormat.Pretty};
+			using (var memory = new MemoryStream())
+			{
+				writer.Write(memory, data);
+				var json = Encoding.UTF8.GetString(memory.ToArray());
+				Assert.AreEqual("{\r\n  \"name\":\"test\"\r\n}", json);
+			}
+		}
+	}
+}
