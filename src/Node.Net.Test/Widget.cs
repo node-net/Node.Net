92354a26897bc10c4a21976d3ead2a613c212f85
lou-parslow <lou.parslow@gmail.com>
2019-01-10 09:20:49 -0700

    added support for ISerializable to JsonWriter

---
+++ src/Node.Net.Test/Widget.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Node.Net.Test
+{
+	class Widget : ISerializable
+	{
+		public Widget() { }
+
+		private Widget(SerializationInfo info, StreamingContext context)
+		{
+			Name = info.GetString("Name");
+			Description = info.GetString("Description");
+		}
+
+		[SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+		public void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			info.AddValue("Name", Name);
+			info.AddValue("Description", Description);
+		}
+
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
+}
--- src/Node.Net.Test/Writer.Test.cs
+++ src/Node.Net.Test/Writer.Test.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
 using System.Text;
 
 namespace Node.Net.Test
@@ -30,6 +32,26 @@ namespace Node.Net.Test
 				writer.Write(memory, data);
 				var json = Encoding.UTF8.GetString(memory.ToArray());
 				Assert.AreEqual("{\r\n  \"name\":\"test\"\r\n}", json);
+			}
+		}
+
+		
+		[Test]
+		public static void WriteISerializableJson()
+		{
+			var widget = new Widget
+			{
+				Name = "abc",
+				Description = "test"
+			};
+
+			using (var memory = new MemoryStream())
+			{
+				new Writer().Write(memory, widget);
+				memory.Seek(0, SeekOrigin.Begin);
+				var json = new StreamReader(memory).ReadToEnd();
+				Assert.True(json.Contains("abc"));
+				Assert.True(json.Contains("test"));
 			}
 		}
 	}
