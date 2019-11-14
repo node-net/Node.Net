91c0ae37531ab62cf8187abcaa59658f0859f9bd
lou-parslow <lou.parslow@gmail.com>
2019-11-14 10:38:20 -0700

    'update'

---
+++ Node.Net/Extension/IFormatter.Extension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Node.Net
+{
+    public static class IFormatterExtension
+    {
+        public static object Clone(this IFormatter formatter, object graph)
+        {
+            using var memory = new MemoryStream();
+            formatter.Serialize(memory, graph);
+            memory.Seek(0, SeekOrigin.Begin);
+            return formatter.Deserialize(memory);
+        }
+
+        public static T Clone<T>(this IFormatter formatter, object graph)
+        {
+            var item = formatter.Clone(graph);
+            if (item is IDictionary dictionary)
+            {
+                return dictionary.Convert<T>();
+            }
+            return (T)item;
+        }
+
+        public static string GetMD5(this IFormatter formatter,object graph)
+        {
+            using var memory = new MemoryStream();
+            formatter.Serialize(memory, graph);
+            memory.Seek(0, SeekOrigin.Begin);
+            return memory.GetMD5String();
+        }
+    }
+}
