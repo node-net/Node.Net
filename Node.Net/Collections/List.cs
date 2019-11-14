ad68aad256d5edbd817d00f630e4cfb58ff1bca9
Lou <lou.parslow@gmail.com>
2019-11-13 17:51:23 -0700

    'update'

---
+++ Node.Net/Collections/List.cs
@@ -0,0 +1,26 @@
+﻿using Node.Net.Internal;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Node.Net.Collections
+{
+	public class List : System.Collections.Generic.List<object>
+	{
+		public string ToJson()
+		{
+			return new JsonWriter().WriteToString(this);
+		}
+
+		public static List Parse(Stream stream)
+		{
+			return new JsonReader { DefaultArrayType = typeof(List) }.Read(stream) as List;
+		}
+
+		public static List Parse(string json)
+		{
+			return Parse(new MemoryStream(Encoding.UTF8.GetBytes(json)));
+		}
+	}
+}
