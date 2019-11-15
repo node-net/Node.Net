3f5916c597347a13ffbbadebb89f06f5fb9807df
lparslow <lou.parslow@gmail.com>
2019-11-15 07:50:33 -0700

    'update'

--- Node.Net/Collections/List.cs
+++ Node.Net/Collections/List.cs
@@ -1,12 +1,14 @@
 ﻿using Node.Net.Internal;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Node.Net.Collections
 {
-	public class List : System.Collections.Generic.List<object>
+	public class List : System.Collections.Generic.List<object> , INotifyPropertyChanged
 	{
 		public string ToJson()
 		{
@@ -22,5 +24,29 @@ namespace Node.Net.Collections
 		{
 			return Parse(new MemoryStream(Encoding.UTF8.GetBytes(json)));
 		}
-	}
+
+        public object? Parent
+        {
+            get { return _parent; }
+            set
+            {
+                if (!object.ReferenceEquals(_parent, value))
+                {
+                    _parent = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private object? _parent;
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
 }
