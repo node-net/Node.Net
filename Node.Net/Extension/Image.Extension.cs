8032ea48a827d95da5a1969632eee1dec9dcacb8
lou-parslow <lou.parslow@gmail.com>
2019-11-20 11:31:56 -0700

    'update'

---
+++ Node.Net/Extension/Image.Extension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Node.Net
+{
+    public static class ImageExtension
+    {
+        public static ImageSource GetImageSource(this System.Drawing.Image image)
+        {
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+
+            var memory = new System.IO.MemoryStream();
+            image.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+            memory.Seek(0, SeekOrigin.Begin);
+
+            bitmapImage.StreamSource = memory;
+            bitmapImage.EndInit();
+            return bitmapImage;
+        }
+    }
+}
