4a5872d77942cccf818093cbeaa66249e98ac06e
lou-parslow <lou.parslow@gmail.com>
2019-11-20 10:49:08 -0700

    'update'

---
+++ Node.Net/Service/WebClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Node.Net.Service
+{
+    public class WebClient : System.Net.WebClient
+    {
+        public int Timeout { get; set; } = 30 * 1000;
+
+        protected override System.Net.WebRequest GetWebRequest(Uri address)
+        {
+            var request = base.GetWebRequest(address);
+            request.Timeout = Timeout;
+            return request;
+        }
+    }
+}
