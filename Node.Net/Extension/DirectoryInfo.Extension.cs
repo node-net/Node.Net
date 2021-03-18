87ca7f70af32386f2e6ab16bc29dad79a1f4bbc4
lou-parslow <lou.parslow@gmail.com>
2021-03-18 12:20:32 -0600

    integrate

---
+++ Node.Net/Extension/DirectoryInfo.Extension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Node.Net
+{
+    public static class DirectoryInfoExtension
+    {
+        public static IEnumerable<DirectoryInfo> GetAncestors(this DirectoryInfo dir)
+        {
+            List<DirectoryInfo> ancestors = new List<DirectoryInfo>();
+            var cur = dir;
+            while (cur != null)
+            {
+                if (cur.Parent is DirectoryInfo parent)
+                {
+                    ancestors.Add(parent);
+                    cur = parent;
+                }
+                else
+                {
+                    cur = null;
+                }
+            }
+            return ancestors;
+        }
+
+        public static DirectoryInfo FindAncestorWithDirectory(this DirectoryInfo dir, string search_pattern)
+        {
+            return dir.GetAncestors().Where(di => di.GetDirectories(search_pattern).Length > 0).FirstOrDefault();
+        }
+    }
+}
---
+++ Node.Net/Extension/MethodInfo.Extension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Node.Net
+{
+    public static class MethodInfoExtension
+    {
+        public static DirectoryInfo GetLogDirectory(this MethodInfo method)
+        {
+            var assemblyDir = new FileInfo(method.DeclaringType!.Assembly.Location).Directory;
+            if (assemblyDir.FindAncestorWithDirectory(".git") is DirectoryInfo projectDir)
+            {
+                return new DirectoryInfo(projectDir.FullName + Path.DirectorySeparatorChar + "log");
+            }
+            return new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) +
+                Path.DirectorySeparatorChar + "log");
+        }
+
+        public static FileInfo GetLogFileInfo(this MethodInfo method, string extension = ".md")
+        {
+            return new FileInfo(method.GetLogDirectory().FullName + Path.DirectorySeparatorChar + extension);
+        }
+    }
+}
