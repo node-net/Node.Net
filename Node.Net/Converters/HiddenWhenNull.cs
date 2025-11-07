afd977c7d311602c96772c5035e87fa8ad7bac1f
lou-parslow <lou.parslow@gmail.com>
2025-11-07 11:07:51 -0700

    integrate

--- Node.Net/Converters/HiddenWhenNull.cs
+++ Node.Net/Converters/HiddenWhenNull.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+#if IS_WINDOWS
+using System;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
@@ -6,7 +8,7 @@ using System.Windows.Data;
 namespace Node.Net.Converters
 {
     /// <summary>
-    /// Converts an object to Visibilty.Hidden when null, otherwise Visibility.Visible
+    /// Converts an object to Visibility.Hidden when null, otherwise Visibility.Visible
     /// </summary>
     public class HiddenWhenNull : IValueConverter
     {
@@ -15,11 +17,6 @@ namespace Node.Net.Converters
         /// <summary>
         /// Convert
         /// </summary>
-        /// <param name="value"></param>
-        /// <param name="targetType"></param>
-        /// <param name="parameter"></param>
-        /// <param name="culture"></param>
-        /// <returns></returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return value == null ? Visibility.Hidden : Visibility.Visible;
@@ -28,14 +25,10 @@ namespace Node.Net.Converters
         /// <summary>
         /// ConvertBack (not implemented)
         /// </summary>
-        /// <param name="value"></param>
-        /// <param name="targetType"></param>
-        /// <param name="parameter"></param>
-        /// <param name="culture"></param>
-        /// <returns></returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
     }
 }
+#endif
