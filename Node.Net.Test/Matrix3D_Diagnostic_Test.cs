13a41568d502846221ac93910de2784eb3d6e261
Lou Parslow <lou.parslow@gmail.com>
2025-11-12 17:25:01 -0700

    integrate

--- Node.Net.Test/Matrix3D_Diagnostic_Test.cs
+++ Node.Net.Test/Matrix3D_Diagnostic_Test.cs
@@ -1,7 +1,7 @@
-#if IS_WINDOWS
 using NUnit.Framework;
 using System.Windows.Media.Media3D;
 using static System.Math;
+using Node.Net;
 
 namespace Node.Net.Test
 {
@@ -16,25 +16,24 @@ namespace Node.Net.Test
             Quaternion q1 = new Quaternion(new Vector3D(0, 0, 1), 15);
             m1.Rotate(q1);
             Vector3D result1 = m1.Transform(new Vector3D(1, 0, 0));
-            
-            System.Console.WriteLine($"Windows Quaternion(0,0,1, 15): X={q1.X}, Y={q1.Y}, Z={q1.Z}, W={q1.W}");
-            System.Console.WriteLine($"Windows Matrix after Rotate: M11={m1.M11}, M12={m1.M12}, M13={m1.M13}");
-            System.Console.WriteLine($"Windows Transform(1,0,0): X={result1.X}, Y={result1.Y}, Z={result1.Z}");
-            
+
+            System.Console.WriteLine($"Quaternion(0,0,1, 15): X={q1.X}, Y={q1.Y}, Z={q1.Z}, W={q1.W}");
+            System.Console.WriteLine($"Matrix after Rotate: M11={m1.M11}, M12={m1.M12}, M13={m1.M13}");
+            System.Console.WriteLine($"Transform(1,0,0): X={result1.X}, Y={result1.Y}, Z={result1.Z}");
+
             // Test 2: Check if Rotate uses Append or Prepend
             Matrix3D m2a = new Matrix3D();
             m2a.Translate(new Vector3D(10, 0, 0));
             m2a.Rotate(new Quaternion(new Vector3D(0, 0, 1), 90));
             Point3D p2a = m2a.Transform(new Point3D(1, 0, 0));
-            System.Console.WriteLine($"Windows Translate then Rotate: Point={p2a.X}, {p2a.Y}, {p2a.Z}");
-            
+            System.Console.WriteLine($"Translate then Rotate: Point={p2a.X}, {p2a.Y}, {p2a.Z}");
+
             // Test 3: RotateOTS behavior
             Matrix3D m3 = new Matrix3D();
             m3.RotateOTS(new Vector3D(15, 0, 0));
             Vector3D xvec3 = m3.Transform(new Vector3D(1, 0, 0));
-            System.Console.WriteLine($"Windows RotateOTS(15,0,0) Transform(1,0,0): X={xvec3.X}, Y={xvec3.Y}, Z={xvec3.Z}");
+            System.Console.WriteLine($"RotateOTS(15,0,0) Transform(1,0,0): X={xvec3.X}, Y={xvec3.Y}, Z={xvec3.Z}");
         }
     }
 }
-#endif
 
