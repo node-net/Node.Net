b155feb0a642077792cb6bede72a1dd653020989
Lou Parslow <lou.parslow@gmail.com>
2025-11-15 07:27:08 -0700

    integrate

---
+++ Node.Net.Test/System/Windows/Media/Color.Tests.cs
@@ -0,0 +1,376 @@
+using System;
+using NUnit.Framework;
+using System.Windows.Media;
+
+namespace Node.Net.Test
+{
+    [TestFixture]
+    internal static class ColorTests
+    {
+        [Test]
+        public static void Constructor_Default_InitializesToZero()
+        {
+            Color color = new Color();
+            Assert.That(color.A, Is.EqualTo(0));
+            Assert.That(color.R, Is.EqualTo(0));
+            Assert.That(color.G, Is.EqualTo(0));
+            Assert.That(color.B, Is.EqualTo(0));
+        }
+
+        [Test]
+        public static void Properties_CanBeSet()
+        {
+            Color color = new Color();
+            color.A = 255;
+            color.R = 100;
+            color.G = 150;
+            color.B = 200;
+            Assert.That(color.A, Is.EqualTo(255));
+            Assert.That(color.R, Is.EqualTo(100));
+            Assert.That(color.G, Is.EqualTo(150));
+            Assert.That(color.B, Is.EqualTo(200));
+        }
+
+        [Test]
+        public static void FromRgb_WithParameters_SetsCorrectValues()
+        {
+            // Arrange & Act
+            Color color = Color.FromRgb(100, 150, 200);
+
+            // Assert
+            Assert.That(color.A, Is.EqualTo(255), "Alpha should be 255 for FromRgb");
+            Assert.That(color.R, Is.EqualTo(100), "Red should be 100");
+            Assert.That(color.G, Is.EqualTo(150), "Green should be 150");
+            Assert.That(color.B, Is.EqualTo(200), "Blue should be 200");
+        }
+
+        [Test]
+        public static void FromRgb_WithZeroValues_SetsCorrectValues()
+        {
+            // Arrange & Act
+            Color color = Color.FromRgb(0, 0, 0);
+
+            // Assert
+            Assert.That(color.A, Is.EqualTo(255));
+            Assert.That(color.R, Is.EqualTo(0));
+            Assert.That(color.G, Is.EqualTo(0));
+            Assert.That(color.B, Is.EqualTo(0));
+        }
+
+        [Test]
+        public static void FromRgb_WithMaxValues_SetsCorrectValues()
+        {
+            // Arrange & Act
+            Color color = Color.FromRgb(255, 255, 255);
+
+            // Assert
+            Assert.That(color.A, Is.EqualTo(255));
+            Assert.That(color.R, Is.EqualTo(255));
+            Assert.That(color.G, Is.EqualTo(255));
+            Assert.That(color.B, Is.EqualTo(255));
+        }
+
+        [Test]
+        public static void FromArgb_WithParameters_SetsCorrectValues()
+        {
+            // Arrange & Act
+            Color color = Color.FromArgb(128, 100, 150, 200);
+
+            // Assert
+            Assert.That(color.A, Is.EqualTo(128), "Alpha should be 128");
+            Assert.That(color.R, Is.EqualTo(100), "Red should be 100");
+            Assert.That(color.G, Is.EqualTo(150), "Green should be 150");
+            Assert.That(color.B, Is.EqualTo(200), "Blue should be 200");
+        }
+
+        [Test]
+        public static void FromArgb_WithZeroAlpha_CreatesTransparentColor()
+        {
+            // Arrange & Act
+            Color color = Color.FromArgb(0, 255, 255, 255);
+
+            // Assert
+            Assert.That(color.A, Is.EqualTo(0), "Alpha should be 0");
+            Assert.That(color.R, Is.EqualTo(255));
+            Assert.That(color.G, Is.EqualTo(255));
+            Assert.That(color.B, Is.EqualTo(255));
+        }
+
+        [Test]
+        public static void FromArgb_WithAllZeroValues_SetsCorrectValues()
+        {
+            // Arrange & Act
+            Color color = Color.FromArgb(0, 0, 0, 0);
+
+            // Assert
+            Assert.That(color.A, Is.EqualTo(0));
+            Assert.That(color.R, Is.EqualTo(0));
+            Assert.That(color.G, Is.EqualTo(0));
+            Assert.That(color.B, Is.EqualTo(0));
+        }
+
+        [Test]
+        public static void FromArgb_WithAllMaxValues_SetsCorrectValues()
+        {
+            // Arrange & Act
+            Color color = Color.FromArgb(255, 255, 255, 255);
+
+            // Assert
+            Assert.That(color.A, Is.EqualTo(255));
+            Assert.That(color.R, Is.EqualTo(255));
+            Assert.That(color.G, Is.EqualTo(255));
+            Assert.That(color.B, Is.EqualTo(255));
+        }
+
+        [Test]
+        public static void EqualityOperator_WithSameValues_ReturnsTrue()
+        {
+            // Arrange
+            Color color1 = Color.FromArgb(255, 100, 150, 200);
+            Color color2 = Color.FromArgb(255, 100, 150, 200);
+
+            // Act & Assert
+            Assert.That(color1 == color2, Is.True);
+        }
+
+        [Test]
+        public static void EqualityOperator_WithDifferentValues_ReturnsFalse()
+        {
+            // Arrange
+            Color color1 = Color.FromArgb(255, 100, 150, 200);
+            Color color2 = Color.FromArgb(255, 101, 150, 200);
+
+            // Act & Assert
+            Assert.That(color1 == color2, Is.False);
+        }
+
+        [Test]
+        public static void EqualityOperator_WithDifferentAlpha_ReturnsFalse()
+        {
+            // Arrange
+            Color color1 = Color.FromArgb(255, 100, 150, 200);
+            Color color2 = Color.FromArgb(128, 100, 150, 200);
+
+            // Act & Assert
+            Assert.That(color1 == color2, Is.False);
+        }
+
+        [Test]
+        public static void InequalityOperator_WithSameValues_ReturnsFalse()
+        {
+            // Arrange
+            Color color1 = Color.FromArgb(255, 100, 150, 200);
+            Color color2 = Color.FromArgb(255, 100, 150, 200);
+
+            // Act & Assert
+            Assert.That(color1 != color2, Is.False);
+        }
+
+        [Test]
+        public static void InequalityOperator_WithDifferentValues_ReturnsTrue()
+        {
+            // Arrange
+            Color color1 = Color.FromArgb(255, 100, 150, 200);
+            Color color2 = Color.FromArgb(255, 101, 150, 200);
+
+            // Act & Assert
+            Assert.That(color1 != color2, Is.True);
+        }
+
+        [Test]
+        public static void Equals_WithSameColor_ReturnsTrue()
+        {
+            // Arrange
+            Color color1 = Color.FromArgb(255, 100, 150, 200);
+            Color color2 = Color.FromArgb(255, 100, 150, 200);
+
+            // Act & Assert
+            Assert.That(color1.Equals(color2), Is.True);
+        }
+
+        [Test]
+        public static void Equals_WithDifferentColor_ReturnsFalse()
+        {
+            // Arrange
+            Color color1 = Color.FromArgb(255, 100, 150, 200);
+            Color color2 = Color.FromArgb(255, 101, 150, 200);
+
+            // Act & Assert
+            Assert.That(color1.Equals(color2), Is.False);
+        }
+
+        [Test]
+        public static void Equals_WithObject_ReturnsTrue()
+        {
+            // Arrange
+            Color color1 = Color.FromArgb(255, 100, 150, 200);
+            object color2 = Color.FromArgb(255, 100, 150, 200);
+
+            // Act & Assert
+            Assert.That(color1.Equals(color2), Is.True);
+        }
+
+        [Test]
+        public static void Equals_WithNull_ReturnsFalse()
+        {
+            // Arrange
+            Color color = Color.FromArgb(255, 100, 150, 200);
+
+            // Act & Assert
+            Assert.That(color.Equals(null), Is.False);
+        }
+
+        [Test]
+        public static void Equals_WithNonColorObject_ReturnsFalse()
+        {
+            // Arrange
+            Color color = Color.FromArgb(255, 100, 150, 200);
+            object obj = "not a color";
+
+            // Act & Assert
+            Assert.That(color.Equals(obj), Is.False);
+        }
+
+        [Test]
+        public static void GetHashCode_WithSameValues_ReturnsSameHashCode()
+        {
+            // Arrange
+            Color color1 = Color.FromArgb(255, 100, 150, 200);
+            Color color2 = Color.FromArgb(255, 100, 150, 200);
+
+            // Act & Assert
+            Assert.That(color1.GetHashCode(), Is.EqualTo(color2.GetHashCode()));
+        }
+
+        [Test]
+        public static void GetHashCode_WithDifferentValues_ReturnsDifferentHashCode()
+        {
+            // Arrange
+            Color color1 = Color.FromArgb(255, 100, 150, 200);
+            Color color2 = Color.FromArgb(255, 101, 150, 200);
+
+            // Act & Assert
+            Assert.That(color1.GetHashCode(), Is.Not.EqualTo(color2.GetHashCode()));
+        }
+
+        [Test]
+        public static void ToString_ReturnsCorrectFormat()
+        {
+            // Arrange
+            Color color = Color.FromArgb(255, 170, 187, 204);
+
+            // Act
+            string result = color.ToString();
+
+            // Assert
+            Assert.That(result, Is.EqualTo("#FFAABBCC"));
+        }
+
+        [Test]
+        public static void ToString_WithZeroValues_ReturnsCorrectFormat()
+        {
+            // Arrange
+            Color color = Color.FromArgb(0, 0, 0, 0);
+
+            // Act
+            string result = color.ToString();
+
+            // Assert
+            Assert.That(result, Is.EqualTo("#00000000"));
+        }
+
+        [Test]
+        public static void ToString_WithMaxValues_ReturnsCorrectFormat()
+        {
+            // Arrange
+            Color color = Color.FromArgb(255, 255, 255, 255);
+
+            // Act
+            string result = color.ToString();
+
+            // Assert
+            Assert.That(result, Is.EqualTo("#FFFFFFFF"));
+        }
+
+#if !IS_WINDOWS
+        [Test]
+        public static void Colors_Black_ReturnsCorrectColor()
+        {
+            // Act
+            Color color = Colors.Black;
+
+            // Assert
+            Assert.That(color.A, Is.EqualTo(255));
+            Assert.That(color.R, Is.EqualTo(0));
+            Assert.That(color.G, Is.EqualTo(0));
+            Assert.That(color.B, Is.EqualTo(0));
+        }
+
+        [Test]
+        public static void Colors_White_ReturnsCorrectColor()
+        {
+            // Act
+            Color color = Colors.White;
+
+            // Assert
+            Assert.That(color.A, Is.EqualTo(255));
+            Assert.That(color.R, Is.EqualTo(255));
+            Assert.That(color.G, Is.EqualTo(255));
+            Assert.That(color.B, Is.EqualTo(255));
+        }
+
+        [Test]
+        public static void Colors_Red_ReturnsCorrectColor()
+        {
+            // Act
+            Color color = Colors.Red;
+
+            // Assert
+            Assert.That(color.A, Is.EqualTo(255));
+            Assert.That(color.R, Is.EqualTo(255));
+            Assert.That(color.G, Is.EqualTo(0));
+            Assert.That(color.B, Is.EqualTo(0));
+        }
+
+        [Test]
+        public static void Colors_Green_ReturnsCorrectColor()
+        {
+            // Act
+            Color color = Colors.Green;
+
+            // Assert
+            Assert.That(color.A, Is.EqualTo(255));
+            Assert.That(color.R, Is.EqualTo(0));
+            Assert.That(color.G, Is.EqualTo(255));
+            Assert.That(color.B, Is.EqualTo(0));
+        }
+
+        [Test]
+        public static void Colors_Blue_ReturnsCorrectColor()
+        {
+            // Act
+            Color color = Colors.Blue;
+
+            // Assert
+            Assert.That(color.A, Is.EqualTo(255));
+            Assert.That(color.R, Is.EqualTo(0));
+            Assert.That(color.G, Is.EqualTo(0));
+            Assert.That(color.B, Is.EqualTo(255));
+        }
+
+        [Test]
+        public static void Colors_Transparent_ReturnsCorrectColor()
+        {
+            // Act
+            Color color = Colors.Transparent;
+
+            // Assert
+            Assert.That(color.A, Is.EqualTo(0));
+            Assert.That(color.R, Is.EqualTo(0));
+            Assert.That(color.G, Is.EqualTo(0));
+            Assert.That(color.B, Is.EqualTo(0));
+        }
+#endif
+    }
+}
+
---
+++ Node.Net/System/Windows/Media/Color.cs
@@ -0,0 +1,139 @@
+using System;
+
+namespace System.Windows.Media
+{
+#if !IS_WINDOWS
+    /// <summary>
+    /// Describes a color in terms of alpha, red, green, and blue channels.
+    /// </summary>
+    public struct Color : IEquatable<Color>
+    {
+        private byte _a;
+        private byte _r;
+        private byte _g;
+        private byte _b;
+
+        /// <summary>
+        /// Gets or sets the sRGB alpha channel value of the color.
+        /// </summary>
+        public byte A
+        {
+            get => _a;
+            set => _a = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the sRGB red channel value of the color.
+        /// </summary>
+        public byte R
+        {
+            get => _r;
+            set => _r = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the sRGB green channel value of the color.
+        /// </summary>
+        public byte G
+        {
+            get => _g;
+            set => _g = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the sRGB blue channel value of the color.
+        /// </summary>
+        public byte B
+        {
+            get => _b;
+            set => _b = value;
+        }
+
+        /// <summary>
+        /// Creates a new Color structure by using the specified sRGB color channel values.
+        /// </summary>
+        /// <param name="r">The red channel, R, of the new color. Values must be between 0 and 255.</param>
+        /// <param name="g">The green channel, G, of the new color. Values must be between 0 and 255.</param>
+        /// <param name="b">The blue channel, B, of the new color. Values must be between 0 and 255.</param>
+        /// <returns>A Color structure with the specified values and an alpha channel value of 255.</returns>
+        public static Color FromRgb(byte r, byte g, byte b)
+        {
+            return new Color { A = 255, R = r, G = g, B = b };
+        }
+
+        /// <summary>
+        /// Creates a new Color structure by using the specified sRGB alpha channel and color channel values.
+        /// </summary>
+        /// <param name="a">The alpha channel, A, of the new color. Values must be between 0 and 255.</param>
+        /// <param name="r">The red channel, R, of the new color. Values must be between 0 and 255.</param>
+        /// <param name="g">The green channel, G, of the new color. Values must be between 0 and 255.</param>
+        /// <param name="b">The blue channel, B, of the new color. Values must be between 0 and 255.</param>
+        /// <returns>A Color structure with the specified values.</returns>
+        public static Color FromArgb(byte a, byte r, byte g, byte b)
+        {
+            return new Color { A = a, R = r, G = g, B = b };
+        }
+
+        /// <summary>
+        /// Tests whether two Color structures are identical.
+        /// </summary>
+        /// <param name="color1">The first Color structure to compare.</param>
+        /// <param name="color2">The second Color structure to compare.</param>
+        /// <returns>true if color1 and color2 are identical; otherwise, false.</returns>
+        public static bool operator ==(Color color1, Color color2)
+        {
+            return color1._a == color2._a && color1._r == color2._r && color1._g == color2._g && color1._b == color2._b;
+        }
+
+        /// <summary>
+        /// Tests whether two Color structures are not identical.
+        /// </summary>
+        /// <param name="color1">The first Color structure to compare.</param>
+        /// <param name="color2">The second Color structure to compare.</param>
+        /// <returns>true if color1 and color2 are not equal; otherwise, false.</returns>
+        public static bool operator !=(Color color1, Color color2)
+        {
+            return !(color1 == color2);
+        }
+
+        /// <summary>
+        /// Tests whether the specified object is a Color structure and is equivalent to this Color.
+        /// </summary>
+        /// <param name="obj">The object to compare to this Color structure.</param>
+        /// <returns>true if obj is a Color structure and is identical to the current Color structure; otherwise, false.</returns>
+        public override bool Equals(object? obj)
+        {
+            return obj is Color color && Equals(color);
+        }
+
+        /// <summary>
+        /// Tests whether the specified Color structure is equivalent to this Color.
+        /// </summary>
+        /// <param name="other">The Color structure to compare to this Color.</param>
+        /// <returns>true if other is identical to this Color structure; otherwise, false.</returns>
+        public bool Equals(Color other)
+        {
+            return _a == other._a && _r == other._r && _g == other._g && _b == other._b;
+        }
+
+        /// <summary>
+        /// Gets a hash code for this Color structure.
+        /// </summary>
+        /// <returns>A hash code for this Color structure.</returns>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_a, _r, _g, _b);
+        }
+
+        /// <summary>
+        /// Creates a string representation of the color using the ARGB channels.
+        /// </summary>
+        /// <returns>The string representation of the Color structure.</returns>
+        public override string ToString()
+        {
+            return $"#{A:X2}{R:X2}{G:X2}{B:X2}";
+        }
+    }
+#endif
+}
+
