f8e1373d806246e9a789e5648f27bb8444532ac6
Lou Parslow <lou.parslow@gmail.com>
2026-01-21 14:33:40 -0700

    integrate

--- scripts/ruby/makit/nuget_ext.rb
+++ scripts/ruby/makit/nuget_ext.rb
@@ -1,7 +1,7 @@
 module Makit
   module NuGetExt
     def self.publish(package, api_key, source)
-      sh "dotnet nuget push #{package} --skip-duplicate --api-key #{api_key} --source #{source}"
+      puts `dotnet nuget push #{package} --skip-duplicate --api-key #{api_key} --source #{source}`
     end
   end
 end
